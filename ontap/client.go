4dc51e264680fcc843695131158b22f9b87aa66a
Daniel Dolev <68538339+dolev7@users.noreply.github.com>
2024-08-05 12:46:51 +0300

    Revert "QtreeNetapp (#1)"
    This reverts commit 7a5e39b4ddac740b3b0130d52687e4e4f2bcc039.

--- ontap/client.go
+++ ontap/client.go
@@ -181,7 +181,6 @@ type Client struct {
 	VolumeUnmount          *VolumeUnmount
 	VserverGetIter         *VserverGetIter
 	VserverShowAggrGetIter *VserverShowAggrGetIter
-	QtreeListIter          *QtreeListIter
 }
 
 type ClientOptions struct {
